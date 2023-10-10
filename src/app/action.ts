f8ac51cb46794b9a00fb2da343ecfa603e39ce87
Umur Deniz Dikmen <117382440+UmurDenizDikmen@users.noreply.github.com>
2023-10-10 13:19:50 +0300

    changefiles

--- src/app/action.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-import { revalidatePath } from "next/cache";
-export default function revalidate() {
-  revalidatePath("/", "page");
-  revalidatePath("/cart", "page");
-  revalidatePath("/products/[id]", "page");
-}
