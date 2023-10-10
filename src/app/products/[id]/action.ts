7478205b9f541e84c42d8a440d6cd97abe856d45
Umur Deniz Dikmen <117382440+UmurDenizDikmen@users.noreply.github.com>
2023-10-10 12:56:47 +0300

    UpdatedActions

--- src/app/products/[id]/action.ts
+++ src/app/products/[id]/action.ts
@@ -3,6 +3,14 @@ import { getCart, createCart } from "@/lib/cart";
 import { prisma } from "@/lib/prismadb";
 import { revalidatePath } from "next/cache";
 
+export const dynamic = "auto";
+export const dynamicParams = true;
+export const revalidate = false;
+export const fetchCache = "auto";
+export const runtime = "nodejs";
+export const preferredRegion = "auto";
+export const maxDuration = 2;
+
 export async function incerementProductQuantity(productId: string) {
   const cart = (await getCart()) ?? (await createCart());
   const articleInCart = cart.items.find((item) => item.productId === productId);
