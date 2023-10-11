d8066ad2ac58ff941c654d48d3020f345a8f0f3b
Umur Deniz Dikmen <117382440+UmurDenizDikmen@users.noreply.github.com>
2023-10-11 11:39:09 +0300

    updatedLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import SessionProvider from "./provider/SessionProvider";
-import { QueryClient, QueryClientProvider } from "react-query";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +10,6 @@ export const metadata = {
   title: "GlowMazon",
   description: "We make your husband cry",
 };
-
-const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -22,15 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <QueryClientProvider client={queryClient}>
-          <SessionProvider>
-            <Navbar />
-            <main className="p-4 max-w-7xl m-auto min-w-[300px]">
-              {children}
-            </main>
-            <Footer />
-          </SessionProvider>
-        </QueryClientProvider>
+        <SessionProvider>
+          <Navbar />
+          <main className="p-4 max-w-7xl m-auto min-w-[300px]">{children}</main>
+          <Footer />
+        </SessionProvider>
       </body>
     </html>
   );
